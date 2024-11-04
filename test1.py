0c81a61d7d42d3f92a53a8298f327bfbdfa5a873
yuri0622 <gimpo.s230064@ggh.goe.go.kr>
2024-11-04 14:26:05 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,8 +6,8 @@ import joblib
 model = joblib.load('logistic_regression_model.pkl') 
 
 # 2.모델 설명
-st.title('_______')
-col1, col2,col3 = st.columns( ____ )      # 몇 개의 컬럼으로 나눌까?
+st.title('합격 분류 에이 전트')
+col1, col2,col3 = st.columns( 3 )      # 몇 개의 컬럼으로 나눌까?
 with col1:
       st.subheader('모델 설명 ')
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
@@ -18,23 +18,23 @@ with col1:
 # 3.데이터시각화
 with col2:
       st.subheader('데이터시각화1')
-      st.image('____________' )   # 이미지 불러오기
+      st.image('시각화1.png' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화2')
-      st.image('____________')    # 이미지 불러오기
+      st.image('시각화2.png')    # 이미지 불러오기
 
 # 4. 모델 활용
 st.subheader('모델 활용')
 st.write('**** 공부시간을 입력하세요.. 인공지능이 당신의 합격/불합격 분류 결과를 알려드립니다!')
 
-a = st.number_input(' _________ ', value=0)   # 사용자 입력
+a = st.number_input(' 공부시간을 입력하세요.. 인공지능이 당신의 합격/ 불합격 분류 결과를 알려드립니다! ', value=0)   # 사용자 입력
 
 if st.button('합불분류'):              # 사용자가 '합불분류' 버튼을 누르면
         input_data = [[ a ]]          # 사용자가 입력한 a,b,c 를 input_data에 저장하고
-        p = model._______(input_data)      # model이 분류한 값을 p에 저장한다
+        p = model.predict(input_data)      # model이 분류한 값을 p에 저장한다
         if p[0] == 1 :
-              st.success('인공지능 분류 결과는 ___입니다')
+              st.success('인공지능 분류 결과는 합격입니다')
         else:
-              st.success('인공지능 분류 결과를 ____입니다')
+              st.success('인공지능 분류 결과를 불합격입니다')
 
               
